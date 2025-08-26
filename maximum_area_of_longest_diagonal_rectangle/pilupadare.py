f30d5ad68b104c46afca61c53871b454d1658fe3
Pilupadare <Kalkidan.ugr-1018-17@aau.edu.et>
2025-08-26 03:21:08 -0700

    Add solution for Maximum Area Of Longest Diagonal Rectangle

---
+++ maximum_area_of_longest_diagonal_rectangle/pilupadare.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def areaOfMaxDiagonal(self, dimensions):
+        """
+        :type dimensions: List[List[int]]
+        :rtype: int
+        """
+        max_diagonal_sq = 0
+        max_area = 0
+
+        for length, width in dimensions:
+            diagonal_sq = length * length + width * width
+            area = length * width
+
+            if diagonal_sq > max_diagonal_sq:
+                max_diagonal_sq = diagonal_sq
+                max_area = area
+            elif diagonal_sq == max_diagonal_sq:
+                max_area = max(max_area, area)
+
+        return max_area
